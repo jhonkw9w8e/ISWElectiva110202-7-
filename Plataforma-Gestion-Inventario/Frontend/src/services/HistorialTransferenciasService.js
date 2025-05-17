b1f66aba2f1e1c6367c0edf42dce48647f0b3c61
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-17 08:59:28 -0500

    thirty-five commit

--- Plataforma-Gestion-Inventario/Frontend/src/services/HistorialTransferenciasService.js
+++ Plataforma-Gestion-Inventario/Frontend/src/services/HistorialTransferenciasService.js
@@ -4,5 +4,19 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 export const getHistorialTransferenciasService = async () => {
   const response = await axios.get(`${API_URL}/movimiento/movimientos/`);
-  return response.data;
+  const data = response.data;
+
+  if (Array.isArray(data)) {
+    return data;
+  }
+
+  if (Array.isArray(data)) {
+    return data.resultados;
+  }
+
+  if (Array.isArray(data)) {
+    return data.movimientos;
+  }
+
+  return [];
 };
