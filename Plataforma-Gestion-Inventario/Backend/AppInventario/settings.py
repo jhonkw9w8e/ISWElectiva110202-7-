df3cbb32140118a5c8c16a456806eb4d8e9ff17e
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-17 15:21:16 -0500

    Update settings.py

--- Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
+++ Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
@@ -52,11 +52,11 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -127,8 +127,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
