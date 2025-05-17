11c100814e0b841deddfc96b4a427dc5afa4019d
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-17 11:20:12 -0500

    update settings.py

--- Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
+++ Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'AppInventario.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'Plataforma_Gestion_Inventario',
-        'USER':'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'NAME':os.getenv('DB_NAME'),
+        'USER':os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT':os.getenv('DB_PORT'),
     }
 }
 
