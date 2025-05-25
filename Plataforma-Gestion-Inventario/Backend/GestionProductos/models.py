99c5c22615a4efafa60e25cb5cc54c587d6b0d33
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-24 21:52:01 -0500

    Update Plataforma Gestion Inventario

---
+++ Plataforma-Gestion-Inventario/Backend/GestionProductos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class CategoriaProducto(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=150)
+    codigo = models.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        help_text="Codigo unico para cada producto"
+    )
+    categoria = models.ForeignKey(CategoriaProducto, on_delete=models.CASCADE,related_name='Productos')
+    descripcion = models.CharField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    stock = models.PositiveIntegerField(default=0)
+    umbral_minimo = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return self.nombre
+    
