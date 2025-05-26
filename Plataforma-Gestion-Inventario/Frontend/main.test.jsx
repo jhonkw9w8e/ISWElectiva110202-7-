93502052bf54f5b7e4a38ccd0d457d28dad96c7a
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-25 20:28:23 -0500

    Create main.test.jsx

---
+++ Plataforma-Gestion-Inventario/Frontend/main.test.jsx
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./src/App";
+import { BrowserRouter } from "react-router-dom";
+
+describe("Main entry renders App Correctly", () => {
+  it("renders App inside BrowserRouter without crashing", () => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+
+    const root = ReactDOM.createRoot(div);
+
+    expect(() => {
+      root.render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      );
+    });
+  });
+});
