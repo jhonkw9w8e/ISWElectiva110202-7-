f6287e0a3618dc000e9cb1fedf5694e1df9cda71
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-25 21:14:12 -0500

    Update vite.config.js

--- Plataforma-Gestion-Inventario/Frontend/vite.config.js
+++ Plataforma-Gestion-Inventario/Frontend/vite.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+  },
 });
