c0521daaa912ca5d8084266662f9f9a3b86d2b81
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-25 20:46:39 -0500

    Update App.test.jsx

--- Plataforma-Gestion-Inventario/Frontend/src/App.test.jsx
+++ Plataforma-Gestion-Inventario/Frontend/src/App.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import App from "../App";
+import App from "../src/App";
 
 describe("App component", () => {
   it("renders the app title", () => {
