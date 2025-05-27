6eb9e6b7adbc54943776050a7a2d3c9f9af4f3cb
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-27 13:49:26 -0500

    Update

--- Plataforma-Gestion-Inventario/Backend/GestionProductos/models/categoria.py
+++ Plataforma-Gestion-Inventario/Backend/GestionProductos/models/categoria.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class CategoriaProducto(models.model):
+class CategoriaProducto(models.Model):
     nombre = models.CharField(max_length=100)
     
     def __str__(self):
