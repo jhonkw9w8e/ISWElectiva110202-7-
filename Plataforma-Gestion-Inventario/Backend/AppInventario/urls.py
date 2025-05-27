0a3c99ea414fe713272083d3825cbb39e672a614
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-27 16:06:25 -0500

    Update urls.py principal

--- Plataforma-Gestion-Inventario/Backend/AppInventario/urls.py
+++ Plataforma-Gestion-Inventario/Backend/AppInventario/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from AppInventario import urls as AppInventario_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(AppInventario_urls))
 ]
