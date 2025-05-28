57832ddaed2d329a1215da9d561985220111b3c6
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-28 07:57:13 -0500

    Add endpoint search

--- Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
+++ Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from ..views.producto import ProductoAPIView, DeshacerElimancionAPIView, ConfirmarEliminacionAPIView
+from ..views.producto import ProductoAPIView, DeshacerElimancionAPIView, ConfirmarEliminacionAPIView, ProductoSearchAPIView
 
 urlpatterns = [
     path('gestion-productos/productos/',ProductoAPIView.as_view(),name='producto-list-create'),
     path('gestion-productos/productos/<int:pk>/', ProductoAPIView.as_view(),name='producto-detail'),
     path('gestion-productos/productos/deshacer-eliminacion/',DeshacerElimancionAPIView.as_view(), name='producto-deshacer-eliminacion'),
-    path('gestion-productos/productos/confirmacion-eliminacion',ConfirmarEliminacionAPIView.as_view(),name='producto-confirmar-eliminacion')
+    path('gestion-productos/productos/confirmacion-eliminacion',ConfirmarEliminacionAPIView.as_view(),name='producto-confirmar-eliminacion'),
+    path('gestion-productos/productos/search/',ProductoSearchAPIView.as_view(),name='producto-search')
 ]
