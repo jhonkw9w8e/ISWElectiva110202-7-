d4d28af964d6b37d0f1eff6e95f5abc578bb67ba
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-28 07:24:24 -0500

    Update urls

--- Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
+++ Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
@@ -2,7 +2,8 @@ from django.urls import path
 from ..views.producto import ProductoAPIView, DeshacerElimancionAPIView, ConfirmarEliminacionAPIView
 
 urlpatterns = [
-    path('/gestion-productos/productos/',ProductoAPIView.as_view()),
-    path('/gestion-productos/productos/deshacer-eliminacion/',DeshacerElimancionAPIView.as_view()),
-    path('/gestion-productps/productos/confirmacion-eliminacion',ConfirmarEliminacionAPIView.as_view())
+    path('gestion-productos/productos/',ProductoAPIView.as_view(),name='producto-list-create'),
+    path('gestion-productos/productos/<int:pk>/', ProductoAPIView.as_view(),name='producto-detail'),
+    path('gestion-productos/productos/deshacer-eliminacion/',DeshacerElimancionAPIView.as_view(), name='producto-deshacer-eliminacion'),
+    path('gestion-productos/productos/confirmacion-eliminacion',ConfirmarEliminacionAPIView.as_view(),name='producto-confirmar-eliminacion')
 ]
