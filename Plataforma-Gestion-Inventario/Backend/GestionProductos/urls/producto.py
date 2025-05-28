ae02bfa9770d2119b6eeb90442dfc8e7b5dc9640
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-28 07:58:08 -0500

    Update url producto

--- Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
+++ Plataforma-Gestion-Inventario/Backend/GestionProductos/urls/producto.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('gestion-productos/productos/',ProductoAPIView.as_view(),name='producto-list-create'),
     path('gestion-productos/productos/<int:pk>/', ProductoAPIView.as_view(),name='producto-detail'),
     path('gestion-productos/productos/deshacer-eliminacion/',DeshacerElimancionAPIView.as_view(), name='producto-deshacer-eliminacion'),
-    path('gestion-productos/productos/confirmacion-eliminacion',ConfirmarEliminacionAPIView.as_view(),name='producto-confirmar-eliminacion'),
+    path('gestion-productos/productos/confirmacion-eliminacion/',ConfirmarEliminacionAPIView.as_view(),name='producto-confirmar-eliminacion'),
     path('gestion-productos/productos/search/',ProductoSearchAPIView.as_view(),name='producto-search')
 ]
