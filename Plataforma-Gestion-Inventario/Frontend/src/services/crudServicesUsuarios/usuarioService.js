251889ad3351436ad33c2c3b1e7c08c5b7cf1fbc
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-29 12:22:37 -0500

    Update service usuarios

--- Plataforma-Gestion-Inventario/Frontend/src/services/crudServicesUsuarios/usuarioService.js
+++ Plataforma-Gestion-Inventario/Frontend/src/services/crudServicesUsuarios/usuarioService.js
@@ -3,21 +3,21 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getUsuarios = () => {
-  return axios.get(`${API_URL}usuarios/${id}/`);
+  return axios.get(`${API_URL}/usuario/usuarios/`);
 };
 
-export const getUsuarioById = () => {
-  return axios.get(`${API_URL}usuarios/`, data);
+export const getUsuarioById = (id) => {
+  return axios.get(`${API_URL}/usuario/usuarios/${id}/`);
 };
 
-export const createUsuario = () => {
+export const createUsuario = (data) => {
+  return axios.post(`${API_URL}/usuario/usuarios/`, data);
+};
+
+export const updateUsuario = (id, data) => {
   return axios.get(`${API_URL}usuarios/${id}/`, data);
 };
 
-export const updateUsuario = () => {
-  return axios.get(`${API_URL}usuarios/${id}/`);
-};
-
-export const deleteUsuario = () => {
+export const deleteUsuario = (id) => {
   return axios.get(`${API_URL}usuarios/${id}/`);
 };
