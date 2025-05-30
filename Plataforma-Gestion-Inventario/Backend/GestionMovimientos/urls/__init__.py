a9b8bdb965862623a11537cd2ebd5deac72f19e4
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-30 18:22:18 -0500

    Update init urls

--- Plataforma-Gestion-Inventario/Backend/GestionMovimientos/urls/__init__.py
+++ Plataforma-Gestion-Inventario/Backend/GestionMovimientos/urls/__init__.py
@@ -2,5 +2,6 @@
 from django.urls import path,include
 from .TipoMovimiento import urlpatterns as TipoMovimiento_urls
 from .MovimientoStock import urlpatterns as MovimientoStock_urls
-
-urlpatterns = TipoMovimiento_urls + MovimientoStock_urls
+from .Almacen import urlpatterns as Almacen_urls
+from .Transferencia import urlpatterns as Transferencia_urls
+urlpatterns = TipoMovimiento_urls + MovimientoStock_urls + Almacen_urls + Transferencia_urls
